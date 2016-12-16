bbbc392f4ef01b36063eb868b5b8f7247b732d5a
r.palmin <roman.palmin@gmail.com>
2016-12-16 15:31:09 +0300

    (js) Рефакторинг

--- src/js/app/models/position.js
+++ src/js/app/models/position.js
@@ -1,4 +1,3 @@
-/*global _, $, underscore*/
 define([], function () {
     return function (options) {
         this.id = options.Id ? options.Id : (options.id ? options.id : '');
@@ -6,15 +5,13 @@ define([], function () {
         this.name = options.Name ? options.Name : '';
         this.description = options.Description ? options.Description : '';
         this.imageUrl = options.ImageUrl ? options.ImageUrl : '';
-        this.currentDiscount = (options.CurrentDiscount ? options.CurrentDiscount.toFixed(0) : 0);// + '%';
-        //this.currentCoin = options.TotalItemsSold ? options.TotalItemsSold : 0;
-        //this.currentCoin = options.TotalItemsSold > 20 ? 20 : options.TotalItemsSold ? options.TotalItemsSold : 0;
+        this.currentDiscount = (options.CurrentDiscount ? options.CurrentDiscount.toFixed(0) : 0);
         this.currentCoin = options.DisplayCoinsNow;
         this.remainToDiscount = (options.RemainToDiscount ? options.RemainToDiscount : 0)/* + ' ед.'*/;
         this.currentDiscountSuperAction = (options.CurrentDiscount ? options.CurrentDiscount.toFixed(0) : 0) + '%';
         this.isExplosive = options.DisplayBadaBoom;
         this.restTime = options.RemainSecondsToFinish;
         this.restTimeToString = this.restTime + ' сек.';
-        this.nextPositionName = options.NextName ? options.NextName : this.name;
+        this.nextPositionName = options.Name2 ? options.Name2 : '?';
     };
 });
